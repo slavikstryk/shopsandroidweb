10610531a9369d1b6ff5702ab9655c399f959aab
Stri_9ujy@student.itstep.org <Stri_9ujy@student.itstep.org>
2021-12-19 19:34:40 +0200

    Editing

--- ShopsAndroidWeb/frontend/src/App.js
+++ ShopsAndroidWeb/frontend/src/App.js
@@ -2,16 +2,25 @@ import React from 'react';
 import Header from './component/header/header';
 import { Route } from 'react-router-dom';
 
-import Main from './component/main/main'
-import Shop from './component/shop/shop'
+import Main from './component/main/main';
+import Shop from './component/shop/shop';
 import Support from './component/support/support';
 
-import SSD240GB from './component/240GB SSD HyperX/ssd240gb';
-import SSD480GB from './component/480GB SSD HyperX/ssd480gb';
-import SSD960GB from './component/960GB SSD HyperX/ssd960gb';
+import SSD240GB from './component/ssd/240GB SSD HyperX/ssd240gb';
+import SSD480GB from './component/ssd/480GB SSD HyperX/ssd480gb';
+import SSD960GB from './component/ssd/960GB SSD HyperX/ssd960gb';
 
-import SSD120GBA400 from './component/120GB SSD Kingston A400/a400120gb'
-import SSD240GBA400 from './component/240GB SSD Kingston A400/a400240gb'
+import SSD120GBA400 from './component/ssd/120GB SSD Kingston A400/a400120gb';
+import SSD240GBA400 from './component/ssd/240GB SSD Kingston A400/a400240gb';
+import SSD480GBA400 from './component/ssd/480GB SSD Kingston A400/a400480gb';
+import SSD960GBA400 from './component/ssd/960GB SSD Kingston A400/a400960gb';
+import SSD1_92TBA400 from './component/ssd/1.92TB SSD Kingston A400/a4001.92tb';
+
+import SSD120GBFURY from './component/ssd/120GB SSD Kingston HyperX Fury/FURY120gb';
+import SSD240GBFURY from './component/ssd/240GB SSD Kingston HyperX Fury/FURY240gb';
+import SSD480GBFURY from './component/ssd/480GB SSD Kingston HyperX Fury/FURY480gb';
+
+import RAM1X4GBFURY from './component/ram/4x1GB 3200 Kingston Fury HyperX/1x4GB3200KingstonFuryHyperX';
 
 function App() {
   return (
@@ -28,6 +37,13 @@ function App() {
         <Route path="/kingston hyperx fury rgb 960 gb" component={SSD960GB}/>
         <Route path="/kingston a 400 120 gb" component={SSD120GBA400}/>
         <Route path="/kingston a 400 240 gb" component={SSD240GBA400}/>
+        <Route path="/kingston a 400 480 gb" component={SSD480GBA400}/>
+        <Route path="/kingston a 400 960 gb" component={SSD960GBA400}/>
+        <Route path="/kingston a 400 1 92 tb" component={SSD1_92TBA400}/>
+        <Route path="/kingston hyperx fury 120 gb" component={SSD120GBFURY}/>
+        <Route path="/kingston hyperx fury 240 gb" component={SSD240GBFURY}/>
+        <Route path="/kingston hyperx fury 480 gb" component={SSD480GBFURY}/>
+        <Route path="/kingston hyperx fury 1 x 4 gb 3200" component={RAM1X4GBFURY}/>
         <Route path="/shop" component={Shop} />
         <Route path="/support" component={Support}/>
       </body>
